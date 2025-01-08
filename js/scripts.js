3cac0f6464f9d83c4f49f1d964a045687b96fc7b
xNyxDev <xnyxdev@gmail.com>
2025-01-08 17:49:44 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,21 @@ hamburger.addEventListener('click', () => {
     nav.classList.toggle('show');
 });
 
- // Lista de imágenes para el fondo
-    const imagenes = [
-        '../img/img/fondos/2025-01-04_19.34.24.png',
-        '../img/img/fondos/2025-01-06_09.51.46.png'
-    ];
+// Lista de imágenes para el fondo
+const imagenes = [
+    '../img/fondos/2025-01-04_19.34.24.png',
+    '../img/fondos/2025-01-06_09.51.46.png'
+];
 
-    let index = 0;
-    const fondo = document.getElementById('fondo');
+let index = 0;
+const fondo = document.getElementById('fondo');
 
-    // Función para cambiar el fondo cada 5 minutos
-    function cambiarFondo() {
-        fondo.style.backgroundImage = `url(${imagenes[index]})`;
-        index = (index + 1) % imagenes.length; // Avanza al siguiente índice y reinicia si es necesario
-    }
+// Función para cambiar el fondo cada 5 minutos
+function cambiarFondo() {
+    fondo.style.backgroundImage = `url(${imagenes[index]})`;
+    index = (index + 1) % imagenes.length; // Avanza al siguiente índice y reinicia si es necesario
+}
 
-    // Cambiar el fondo inmediatamente y luego cada 5 minutos (300000 ms)
-    cambiarFondo();
-    setInterval(cambiarFondo, 300000);
+// Cambiar el fondo inmediatamente y luego cada 5 minutos (300000 ms)
+cambiarFondo();
+setInterval(cambiarFondo, 300000);
